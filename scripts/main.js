fcf9e0fb325fb54e1090b20e7a8bd1e2aae07532
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-07 01:49:57 +0200

    rebuild(main.js): boolean values instead of 1 and 0 used to clarify the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 const container = document.getElementById('fizzbuzz-container');
 
-for (let i = 1 , isPrime = 1; i <= 100; i++ , isPrime = 1) {
+for (let i = 1 , isPrime = true; i <= 100; i++ , isPrime = true) {
     const newDiv = document.createElement('div');
 
     if(i % 3 === 0) {
         newDiv.classList.add('fizz');
         newDiv.append('Fizz');
-        isPrime = 0;
+        isPrime = false;
     }
     
     if(i % 5 === 0) {
         newDiv.classList.add('buzz');
         newDiv.append('Buzz');
-        isPrime = 0;
+        isPrime = false;
     }
 
     // If boolean is unchanged i is not a multiple
